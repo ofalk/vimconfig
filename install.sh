2250b69c1a24848e236439645968a4024145a105
Oliver Falk <oliver.falk@omv.com>
2015-04-15 10:59:50 +0300

    Add install script to ease the installation

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git clone -q https://github.com/ofalk/vimconfig.git vimconfig.ofalk.git
+pushd vimconfig.ofalk.git
+if [ -e ~/.vim ]; then
+	mv ~/.vim ~/.vim.old
+fi
+ln -sf `pwd`/.vim ~/.vim
+if [ -e ~/.vimrc ]; then
+	mv ~/.vimrc ~/.vimrc.old
+fi
+ln -sf `pwd`/.vimrc ~/.vimrc
+popd
+vim +BundleInstall
